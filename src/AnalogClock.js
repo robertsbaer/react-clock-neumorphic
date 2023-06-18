34284ad232f655e981e9728c3b4816da3add9405
robertsbaer <robertsbaer@yahoo.com>
2023-06-18 14:26:10 -0400

    modified:   package-lock.json modified:   package.json modified:   public/favicon.ico new file:   public/favicoon.ico modified:   public/index.html modified:   src/AnalogClock.css modified:   src/AnalogClock.js modified:   src/App.js new file:   src/styles.css

--- src/AnalogClock.js
+++ src/AnalogClock.js
@@ -1,43 +1,47 @@
-import React, { useEffect, useState } from 'react';
-import './AnalogClock.css';
+import React, { useEffect, useState } from "react";
+import styled, { keyframes } from "styled-components";
+import "./styles.css";
 
-const AnalogClock = () => {
-  const [time, setTime] = useState(new Date());
+const rotate = (degree) => keyframes`
+  from {
+    transform: translate(-50%, -100%) rotate(${degree}deg);
+  }
+  to {
+    transform: translate(-50%, -100%) rotate(${degree + 360}deg);
+  }
+`;
+
+const ClockStick = styled.div`
+  animation: ${(props) => rotate(props.degree)} ${(props) => props.seconds}s
+    linear infinite;
+`;
+
+const App = () => {
+  const [degrees, setDegrees] = useState({});
 
   useEffect(() => {
-    const interval = setInterval(() => {
-      setTime(new Date());
-    }, 1000);
-
-    return () => {
-      clearInterval(interval);
-    };
+    setDegrees({
+      hour: getRotation("Hours", 12),
+      minute: getRotation("Minutes", 60),
+      second: getRotation("Seconds", 60)
+    });
   }, []);
 
   const getRotation = (unit, max) => {
+    const time = new Date();
     const value = time[`get${unit}`]();
-    let rotation = (value * 360) / max;
-
-    if (unit === 'Seconds') {
-      if (value === 59) {
-        const milliseconds = time.getMilliseconds();
-        rotation += ((1000 - milliseconds) / 1000) * 6;
-      }
-    }
-
-    return {
-      transform: `translate(-50%, -100%) rotate(${rotation}deg)`,
-    };
+    const rotation = (value * 360) / max;
+    return rotation;
   };
 
   const renderNumbers = () => {
     const numbers = [];
 
     for (let i = 1; i <= 12; i++) {
-        const angle = (i * 30) * (Math.PI / 180);      
-        const numberStyle = {
+      const angle = i * 30 * (Math.PI / 180);
+      const numberStyle = {
         left: `calc(50% + ${Math.sin(angle) * 140}px)`,
-        top: `calc(50% - ${Math.cos(angle) * 140}px)`,
+        top: `calc(50% - ${Math.cos(angle) * 140}px)`
       };
       numbers.push(
         <div key={i} className="number" style={numberStyle}>
@@ -52,12 +56,24 @@ const AnalogClock = () => {
   return (
     <div className="clock">
       {renderNumbers()}
-      <div className="hour-hand" style={getRotation('Hours', 12)}></div>
-      <div className="minute-hand" style={getRotation('Minutes', 60)}></div>
-      <div className="second-hand" style={getRotation('Seconds', 60)}></div>
+      <ClockStick
+        className="hour-hand"
+        seconds={216000}
+        degree={degrees.hour}
+      ></ClockStick>
+      <ClockStick
+        className="minute-hand"
+        seconds={3600}
+        degree={degrees.minute}
+      ></ClockStick>
+      <ClockStick
+        className="second-hand"
+        seconds={60}
+        degree={degrees.second}
+      ></ClockStick>
       <div className="center-circle"></div>
     </div>
   );
 };
 
-export default AnalogClock;
+export default App;
--- src/App.js
+++ src/App.js
@@ -1,13 +1,77 @@
-import React from 'react';
-import AnalogClock from './AnalogClock';
-import './App.css';
+import React, { useEffect, useState } from "react";
+import styled, { keyframes } from "styled-components";
+import "./styles.css";
+
+const rotate = (degree) => keyframes`
+  from {
+    transform: translate(-50%, -100%) rotate(${degree}deg);
+  }
+  to {
+    transform: translate(-50%, -100%) rotate(${degree + 360}deg);
+  }
+`;
+
+const ClockStick = styled.div`
+  animation: ${(props) => rotate(props.degree)} ${(props) => props.seconds}s
+    linear infinite;
+`;
 
 const App = () => {
+  const [degrees, setDegrees] = useState({});
+
+  useEffect(() => {
+    setDegrees({
+      hour: getRotation("Hours", 12),
+      minute: getRotation("Minutes", 60),
+      second: getRotation("Seconds", 60)
+    });
+  }, []);
+
+  const getRotation = (unit, max) => {
+    const time = new Date();
+    const value = time[`get${unit}`]();
+    const rotation = (value * 360) / max;
+    return rotation;
+  };
+
+  const renderNumbers = () => {
+    const numbers = [];
+
+    for (let i = 1; i <= 12; i++) {
+      const angle = i * 30 * (Math.PI / 180);
+      const numberStyle = {
+        left: `calc(50% + ${Math.sin(angle) * 140}px)`,
+        top: `calc(50% - ${Math.cos(angle) * 140}px)`
+      };
+      numbers.push(
+        <div key={i} className="number" style={numberStyle}>
+          {i}
+        </div>
+      );
+    }
+
+    return numbers;
+  };
+
   return (
-    <div className="app">
-      <div className="center-container">
-        <AnalogClock />
-      </div>
+    <div className="clock">
+      {renderNumbers()}
+      <ClockStick
+        className="hour-hand"
+        seconds={216000}
+        degree={degrees.hour}
+      ></ClockStick>
+      <ClockStick
+        className="minute-hand"
+        seconds={3600}
+        degree={degrees.minute}
+      ></ClockStick>
+      <ClockStick
+        className="second-hand"
+        seconds={60}
+        degree={degrees.second}
+      ></ClockStick>
+      <div className="center-circle"></div>
     </div>
   );
 };
